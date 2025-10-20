6669af6de08fbbb327d4ae85b18ebe33ea38b44e
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-10-20 09:31:40 +0530

    added some more elemnts in js for local storage .

--- 012 Project 3 - Todo List App using Pure HTML, CSS & JavaScript/app.js
+++ 012 Project 3 - Todo List App using Pure HTML, CSS & JavaScript/app.js
@@ -1,6 +1,10 @@
 const addTodo = document.getElementById("addTodoBtn");
 const inputTag=document.getElementById("todoInput")
 let todoText;
+let todoString = localStorage.getItem("todos");
+if(todoString){
+    todos = JSON.parse(todoString);
+}
 let todos=[];
 
 addTodo.addEventListener("click", ()=>{
@@ -8,4 +12,10 @@ addTodo.addEventListener("click", ()=>{
     todoText = inputTag.value
     console.log(todoText)
     inputTag.value = "";
+    let todo={
+        title: todoText,
+        isCompleted: false
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
 });
