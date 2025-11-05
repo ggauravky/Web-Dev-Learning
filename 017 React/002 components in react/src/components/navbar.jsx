c78378a3f2a42622adf92ab930ea78ef8b40e406
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-11-05 14:38:29 +0530

    learned components of react

---
+++ 017 React/002 components in react/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './navbar.css'
+
+const navbar = () => {
+  return (
+    <div className='navbar'><ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+       </ul>
+    </div>
+  )
+}
+
+export default navbar
