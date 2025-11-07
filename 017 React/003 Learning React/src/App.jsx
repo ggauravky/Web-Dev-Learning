bc8773fe2c31c2bc6f8ac8686ee71e2a9f21e479
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-11-07 18:32:59 +0530

    learned about States in React

--- 017 React/003 Learning React/src/App.jsx
+++ 017 React/003 Learning React/src/App.jsx
@@ -5,20 +5,33 @@ import './App.css'
 import Navbar from './components/navbar.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("hey i am a react developer")
+
+  const btn1 = () => {
+    setText("you clicked button 1")
+  }
+
+  const btn2 = () => {
+    setText("you clicked button 2")
+  }
 
   return (
     <>
     <Navbar logotext="Gaurav"/>
       <div>
-        
+
+          <div>{text}</div>
+          <div>
+            <button onClick={btn1}>button 1</button>
+            ,<br/>
+
+            <button onClick={btn2}>button 2</button>
+          </div>
         
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
