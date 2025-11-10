70e00eeeb60b7c8f2c6e32e405ce44e6cff299ab
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-11-10 11:19:02 +0530

    Initialize React Notes App project structure
    Add initial files for a React-based notes app, including components, configuration, and assets. Sets up basic Navbar and Card components, project README, ESLint config, and Vite setup.

---
+++ 018 Project 04 Notes App Using React/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    
+    <nav>
+      <div className="logo">Gaurav's Notes</div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ 018 Project 04 Notes App Using React/Components/card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = ({ title, desc }) => {
+  return (
+    <div>card title is {title} and description is {desc}</div>
+  )
+}
+
+export default Card
